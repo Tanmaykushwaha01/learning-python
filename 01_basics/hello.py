d1e43b4e84a7bd695ba6a17af235fbaf1015d48e
Tanmaykushwaha01 <tanmaykushwaha0101@gmail.com>
2025-04-01 02:03:37 +0530

    python in shell

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -3,4 +3,9 @@ print("hello world")
 def chai(n):
     print(n)
 
-chai("hey my name is tanmay")
+chai("hey, my name is tanmay")
+
+
+chai_one = "milk tea"
+chai_two = "lemon tea"
+chai_three = "ginger tea"
